99676df52ea5c288ab802fe3fb229ae0724ec6e1
J. Andrew Brassington <jabbrass@zoho.com>
2014-08-26 17:30:00 -0700

    Added github avatar and username on left with reponame on top

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var port = process.env.PORT || 3000;
 var github = new GitHubApi({
 	version: "3.0.0"
 });
+github.authenticate({
+    type: "oauth",
+    token: "bae8b561a5aa33a712bfe9546b3a6957ab186fd9"
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -18,6 +22,14 @@ app.use(partials());
 
 app.get('/:user/:repo', function(req, res) {
 	req.setEncoding('utf8');
+	var user;
+			github.user.get({username: req.params.user}, function(error, data) {
+			if (error) {
+				console.log(error);
+			} else {
+        user = data;
+      }
+		})
 	github.repos.getContent({
 		user: req.params.user,
 		repo: req.params.repo,
@@ -27,6 +39,7 @@ app.get('/:user/:repo', function(req, res) {
 		if (error) {
 			console.log(error);
 		}
+
 		var buffer = new Buffer(data.content, 'base64');
 		var randId = uuid.v4();
 		var tempUrl = '/tmp/'+randId+'.js';
@@ -36,7 +49,11 @@ app.get('/:user/:repo', function(req, res) {
 			} else {
 				console.log("The file was changed.");
 				res.render('index', { layout: true, locals: {
-					scripturl: tempUrl
+					scripturl: tempUrl,
+					avatarUrl: user.avatar_url,
+					username: user.login,
+					fullname: user.name,
+					reponame: req.params.repo
 				}});
 			}
 		})
